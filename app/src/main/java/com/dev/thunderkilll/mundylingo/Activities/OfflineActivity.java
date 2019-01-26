4f26df34ca0cc3a1b1d850b3bf3bc5580c0839f1
thunderkill <khaled.guesmi@esprit.tn>
2019-01-26 19:28:37 +0100

    offline Activity + layout : today i'll be adding the offline activity and layouts

---
+++ app/src/main/java/com/dev/thunderkilll/mundylingo/Activities/OfflineActivity.java
@@ -0,0 +1,15 @@
+package com.dev.thunderkilll.mundylingo.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dev.thunderkilll.mundylingo.R;
+
+public class OfflineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offline);
+    }
+}
